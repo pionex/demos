cc6d5b201331dbb719cdf874f5e673c6054fd097
Matt Middleton <mmiddleton@wynright.com>
2021-08-12 16:02:31 +1200

    listener only

--- demo_nodes_cpp/launch/topics/talker_listener.launch.py
+++ demo_nodes_cpp/launch/topics/talker_listener.launch.py
@@ -20,8 +20,8 @@ import launch_ros.actions
 
 def generate_launch_description():
     return LaunchDescription([
-        launch_ros.actions.Node(
-            package='demo_nodes_cpp', executable='talker', output='screen'),
+#        launch_ros.actions.Node(
+#            package='demo_nodes_cpp', executable='talker', output='screen'),
         launch_ros.actions.Node(
             package='demo_nodes_cpp', executable='listener', output='screen'),
     ])
